d7d3837ced2d3a0c7e4b59a7e67522dc325f86e4
jieming <zhuotianrun.ztr@alibaba-inc.com>
2026-02-09 15:37:10 +0800

    chore(vite.config.js): 添加路径别名配置
    refactor(home/index.scss): 调整样式选择器结构

--- h5-mall/vite.config.js
+++ h5-mall/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from '@vant/auto-import-resolver'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,11 @@ export default defineConfig({
       resolvers: [VantResolver()]
     })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   server: {
     port: 3000,
     host: '0.0.0.0'
